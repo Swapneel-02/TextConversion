f4b8db92497235cc6781b89e62115e8447c86b28
Swapneel Das <swapneel155@gmail.com>
2023-04-03 02:15:33 +0530

    Making textconversion using react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+function App() {
+  return (
+   <>
+   <Navbar title="TextUtils" />
+   <div className="container my-3">
+   <TextForm heading="Enter your text here:"/>
+   </div>
+   </>
+  );
+}
+
+export default App;
