d8c5324a4fd18e1d1aa59a3099f3d3a161361625
Swapneel Das <swapneel155@gmail.com>
2023-04-08 17:13:34 +0530

    Added route in my website

--- src/App.js
+++ src/App.js
@@ -2,21 +2,61 @@ import React, { useState } from 'react';
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import Alert from './components/Alert';
+import About from './components/About';
 
-
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+  } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  
+  const showAlert=(message,type) => {
+       setAlert({
+          msg: message,
+          type: type
+       })
+       setTimeout(() =>{
+         setAlert(null);    
+       }, 2000);
+  }
+
   const toggleMode =() =>{
     setMode(mode === 'dark' ? 'light' : 'dark');
-    document.body.style.backgroundColor=(mode === 'dark' ? 'white' : 'black');
+    document.body.style.backgroundColor=(mode === 'dark' ? 'white' : '#02204a');
+    if(mode === 'light'){
+      showAlert('Dark mode has been enabled',"success");
+    }
+    else{
+      showAlert('Light mode has been enabled',"success");
+    }
   }
+  // const changeMode =() => {
+  //    if (mode==='danger'){
+  //     showAlert('Red mode has been enabled',"danger");
+  //     document.body.style.backgroundColor='red';
+  //    }
+  //    else if( mode==='success'){
+  //     showAlert('Green mode has been enabled',"success");
+  //     document.body.style.backgroundColor='green';
+  //    }
   return (
     <>
+    <Router>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter your text here:" mode={mode}/>
+       <Routes>
+        <Route path="/About" element={<About/>} />
+        <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter your text here:" mode={mode}/>} />
+       </Routes>       
       </div> 
+      </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,29 +21,36 @@ export default function TextForm(props) {
       var text = document.getElementById("myBox");
       text.select();
       navigator.clipboard.writeText(text.value);
+      props.showAlert("Text copied to Clipboard","success");
     }
     const handleExtraSpaces =() => {
        let newText=text.split(/[ ]+/);
        setText(newText.join(" "))
     }
     const [text,setText] = useState('');
+     
+    function getCount(str) {
+      return str.split(' ').filter(function(num) {
+       return num !== ''
+      }).length;
+     }
   return (
     <>
     <div>
         <div className="container" style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
         <h1>{props.heading}</h1> 
         <div className="mb-3">
-        <textarea className="form-control" id="myBox" value={text} style={{backgroundColor: props.mode === 'dark' ? 'black' : 'white', color: props.mode === 'dark' ? 'white' : 'black'}} onChange={handleOnChange} rows="8"></textarea>
+        <textarea className="form-control" id="myBox" value={text} style={{backgroundColor: props.mode === 'dark' ? '#212529' : 'white', color: props.mode === 'dark' ? 'white' : 'black'}} onChange={handleOnChange} rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
-        <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to lowercase</button>
-        <button className="btn btn-primary mx-1.5" onClick={handleClearClick}>Clear Text</button>
-        <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
-        <button className="btn btn-primary mx-1.5" onClick={handleExtraSpaces}>Remove extra spaces</button>
+        <button className="btn btn-outline-primary" onClick={handleUpClick}>Convert to uppercase</button>
+        <button className="btn btn-outline-primary mx-2" onClick={handleLowClick}>Convert to lowercase</button>
+        <button className="btn btn-outline-primary mx-1.5" onClick={handleClearClick}>Clear Text</button>
+        <button className="btn btn-outline-primary mx-2" onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-outline-primary mx-1.5" onClick={handleExtraSpaces}>Remove extra spaces</button>
     </div>
     <div className="container" my-2 style={{color: props.mode === 'dark' ? 'white' : 'black'}}>
        <h2>Your text summary</h2>
-       <p>{text.split(" ").length} words and {text.length} characters</p>   
+       <p>{getCount(text)} words and {text.length} characters</p>   
        <h3>Preview</h3>
        <p>{text}</p>
     </div>  
